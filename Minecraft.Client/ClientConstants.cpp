ab735e5b2e1aa9e5b4beb466a642918ba6f61e06
Night <106701321+VortexVirus@users.noreply.github.com>
2026-03-11 18:04:07 +0100

    adding my build for ps3 with little fixes
    truc cool

---
+++ Minecraft.Client/ClientConstants.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "ClientConstants.h"
+
+#if (defined __PS3__)
+				const wstring ClientConstants::VERSION_STRING = wstring(L"Minecraft PS3 ") + VER_FILEVERSION_STR_W;//+ SharedConstants::VERSION_STRING;
+#elif (defined _XBOX)
+				const wstring ClientConstants::VERSION_STRING = wstring(L"Minecraft Xbox ") + VER_FILEVERSION_STR_W;//+ SharedConstants::VERSION_STRING;
+#elif (defined __ORBIS__)
+				const wstring ClientConstants::VERSION_STRING = wstring(L"Minecraft Orbis ") + VER_FILEVERSION_STR_W;//+ SharedConstants::VERSION_STRING;
+#elif (defined _DURANGO)
+				const wstring ClientConstants::VERSION_STRING = wstring(L"Minecraft Durango ") + VER_FILEVERSION_STR_W;//+ SharedConstants::VERSION_STRING;
+#elif (defined __PSVITA__)
+				const wstring ClientConstants::VERSION_STRING = wstring(L"Minecraft PSVita ") + VER_FILEVERSION_STR_W;//+ SharedConstants::VERSION_STRING;
+#endif
+//|| defined __ORBIS__  || defined _DURANGO  || defined __PSVITA__
+//const wstring ClientConstants::VERSION_STRING = wstring(L"Minecraft Xbox ") + VER_FILEVERSION_STR_W;//+ SharedConstants::VERSION_STRING;
